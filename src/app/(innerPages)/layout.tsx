1a10aaab9f6439886c8b2f55d14bfeba7dd4d4b3
takashi <tks.srtr89@gmail.com>
2024-08-19 21:30:16 +0900

    delete

--- src/app/(innerPages)/layout.tsx
+++ src/app/(innerPages)/layout.tsx
@@ -1,12 +1,6 @@
 'use client'
 
 import { AuthWrapper } from '@/components/AuthWrapper'
-import { authToken } from '@/globalState'
-import { auth, getCurrentUser } from '@/utils'
-import { signOut } from 'firebase/auth'
-import { useAtomValue } from 'jotai'
-import { Content } from 'next/font/google'
-import { useRouter } from 'next/navigation'
 import { ReactNode } from 'react'
 
 type Props = {
