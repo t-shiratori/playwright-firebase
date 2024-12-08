8c9d0252bc0f138dffefe26af140cbaf77a1b5ba
takashi <tks.srtr89@gmail.com>
2024-12-08 17:56:47 +0900

    update

--- e2e-tests/pages/fixtureExecutionOrder.spec.ts
+++ e2e-tests/pages/fixtureExecutionOrder.spec.ts
@@ -4,58 +4,101 @@
 
 import { test as base } from '@playwright/test'
 
-const test = base.extend<
-	{
-		testFixture: string
-		autoTestFixture: string
-		unusedFixture: string
-	},
-	{
-		workerFixture: string
-		autoWorkerFixture: string
+type WorkerFixture = {
+	workerFixture: string
+	autoWorkerFixture: string
+}
+
+type TestFixture = {
+	autoTestFixture1: string
+	autoTestFixture2: string
+	manualTestFixture1: string
+	manualTestFixture2: {
+		print: () => void
 	}
->({
+	unusedFixture: string
+}
+
+const test = base.extend<TestFixture, WorkerFixture>({
 	workerFixture: [
 		async ({ browser }, use) => {
-			// workerFixture setup...
+			console.log('>> workerFixture > set up')
+
 			await use('workerFixture')
-			// workerFixture teardown...
+
+			console.log('>> workerFixture > tear down')
 		},
 		{ scope: 'worker' },
 	],
 
 	autoWorkerFixture: [
 		async ({ browser }, use) => {
-			// autoWorkerFixture setup...
+			console.log('>> autoWorkerFixture > set up')
+
 			await use('autoWorkerFixture')
-			// autoWorkerFixture teardown...
+
+			console.log('>> autoWorkerFixture > tear down')
 		},
 		{ scope: 'worker', auto: true },
 	],
 
-	testFixture: [
-		async ({ page, workerFixture }, use) => {
-			// testFixture setup...
-			await use('testFixture')
-			// testFixture teardown...
-		},
-		{ scope: 'test' },
-	],
+	autoTestFixture1: [
+		async ({ autoTestFixture2 }, use) => {
+			console.log('>> autoTestFixture1 > set up')
 
-	autoTestFixture: [
-		async ({}, use) => {
-			// autoTestFixture setup...
-			await use('autoTestFixture')
-			// autoTestFixture teardown...
+			await use('autoTestFixture1')
+
+			console.log('>> autoTestFixture1 > tear down')
 		},
 		{ scope: 'test', auto: true },
 	],
 
+	autoTestFixture2: [
+		async ({}, use) => {
+			console.log('>> autoTestFixture2 > set up')
+
+			await use('autoTestFixture2')
+
+			console.log('>> autoTestFixture2 > tear down')
+		},
+		{ scope: 'test', auto: true },
+	],
+
+	manualTestFixture1: [
+		async ({}, use) => {
+			console.log('>> manualTestFixture1 > set up')
+
+			await use('manualTestFixture1')
+
+			console.log('>> manualTestFixture1 > tear down')
+		},
+		{ scope: 'test', auto: false },
+	],
+
+	manualTestFixture2: [
+		async ({}, use) => {
+			const manualTestFixture2 = {
+				print: () => {
+					console.log('>> printed manualTestFixture2')
+				},
+			}
+
+			console.log('>> manualTestFixture2 > set up')
+
+			await use(manualTestFixture2)
+
+			console.log('>> manualTestFixture2 > tear down')
+		},
+		{ scope: 'test', auto: false },
+	],
+
 	unusedFixture: [
 		async ({ page }, use) => {
-			// unusedFixture setup...
+			console.log('>> unusedFixture > set up')
+
 			await use('unusedFixture')
-			// unusedFixture teardown...
+
+			console.log('>> unusedFixture > tear down')
 		},
 		{ scope: 'test' },
 	],
@@ -69,13 +112,10 @@ test.beforeEach(async ({ page }) => {
 	/* ... */
 	console.log('>> beforeEach')
 })
-test('first test', async ({ page }) => {
+test('test1', async ({ page, manualTestFixture2 }) => {
 	/* ... */
-	console.log('>> first test')
-})
-test('second test', async ({ testFixture }) => {
-	/* ... */
-	console.log('>> second test')
+	console.log('>> test1')
+	manualTestFixture2.print()
 })
 test.afterEach(async () => {
 	/* ... */
